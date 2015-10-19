7bee121cc3047bac808df77eba6da468150c491f
liudanking <liudanking@gmail.com>
2015-10-19 11:53:25 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 easy_install supervisor
 mkdir -p /etc/supervisor/conf.d
-wget conf -O /etc/supervisor/supervisord.conf
-wget https://gist.github.com/howthebodyworks/176149/raw/88d0d68c4af22a7474ad1d011659ea2d27e35b8d/supervisord.sh -O /etc/init.d/supervisord
+wget https://raw.githubusercontent.com/liudanking/supervisor-install/master/supervisord.conf -O /etc/supervisor/supervisord.conf
+wget https://raw.githubusercontent.com/liudanking/supervisor-install/master/supervisord.rc -O /etc/init.d/supervisord
 chmod +x /etc/init.d/supervisord
 update-rc.d supervisord defaults
