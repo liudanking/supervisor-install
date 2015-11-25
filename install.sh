7eb1abc066a20414e1bde75de5be2763c785812c
Danieliu <liudanking@gmail.com>
2015-11-25 14:36:00 +0800

    start service after installation

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 apt-get install python-setuptools -y
 easy_install supervisor
 mkdir -p /etc/supervisor/conf.d
+mkdir -p /var/log/supervisor
 wget https://raw.githubusercontent.com/liudanking/supervisor-install/master/supervisord.conf -O /etc/supervisor/supervisord.conf
 wget https://raw.githubusercontent.com/liudanking/supervisor-install/master/supervisord.rc -O /etc/init.d/supervisord
 chmod +x /etc/init.d/supervisord
 update-rc.d supervisord defaults
+
+# start service
+service supervisord start
